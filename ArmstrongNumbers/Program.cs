24c59e2781c044d38f3138a6ed91c0fcb9cb69c0
Vesko <vesiv99@abv.bg>
2019-09-26 10:18:12 +0300

    ArmstrongNumberFinder

---
+++ ArmstrongNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArmstrongNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number and the console will tell you if it is Armstrong");
+            int input = int.Parse(Console.ReadLine());
+            int inputOriginal = input;
+            List<int> inputByDigits = new List<int>();
+            double sumOfDigits = 0;
+
+            while (input > 0)
+            {
+                inputByDigits.Add(input % 10);
+                input = input / 10;
+            }
+
+            for (int i = 0; i < inputByDigits.Count; i++)
+            {
+                sumOfDigits+=Math.Pow(inputByDigits[i],inputByDigits.Count);
+            }
+
+            if (sumOfDigits==inputOriginal)
+            {
+                Console.WriteLine("This is an Armstrong number");
+            }
+            else Console.WriteLine("This is not an Armstrong number");
+        }
+    }
+}
