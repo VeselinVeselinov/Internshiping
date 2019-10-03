0e65f00c970089d76addb9dfd80e0882bdf461bb
Vesko <vesiv99@abv.bg>
2019-10-03 17:19:16 +0300

    status updated

---
+++ ToDoListStructure/Entities/Consumers/AccountStatus.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoListStructure.Entities.Consumers
+{
+    class AccountStatus
+    {
+    }
+}
---
+++ ToDoListStructure/Entities/Notes/NoteStatus.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoListStructure.Entities.Notes
+{
+    class NoteStatus
+    {
+    }
+}
